b2eb1f34b0a026874a2ec857de526d6bdb60994d
Bo <bshubinsky@gmail.com>
2023-05-18 12:35:54 -0400

    Update variables.tf

--- Terraform/Azure_Policy_Assignment/variables.tf
+++ Terraform/Azure_Policy_Assignment/variables.tf
@@ -2,6 +2,7 @@ variable "stored_management_group_id" {
   type = string
 }
 
+## This needs to be the full address i.e. /providers/Microsoft.Management/managementGroups/MGNAME
 variable "applied_scope_id" {
   type = string
 }
