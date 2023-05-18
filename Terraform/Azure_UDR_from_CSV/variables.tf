8c1e4718a10f4fe1b146ac6ac6209c32073d1fbd
Bo <bshubinsky@gmail.com>
2023-05-18 12:33:25 -0400

    Create variables.tf

---
+++ Terraform/Azure_UDR_from_CSV/variables.tf
@@ -0,0 +1,10 @@
+variable rg_name {
+    type = string
+    description = "Name of the resource group the existing route table exists in"
+}
+
+variable rt_name {
+    type = string
+    description = "Name of the existing route table"
+}
+
